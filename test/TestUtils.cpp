fe4ce55b6f36844b1db141eeb54f5ccf4b69e995
Puneet Agrawal <agrwalpuneet@gmail.com>
2016-07-06 16:01:05 +0530

    single threaded performance test

---
+++ test/TestUtils.cpp
@@ -0,0 +1,82 @@
+/**@file	MatchingEngine/test/TestUtils.cpp
+ * @author	puneet
+ * @version	704
+ * @date
+ * 	Created:	Wed Jul  6 15:28:04 2016 \n
+ * 	Last Update:	Wed Jul  6 15:28:04 2016
+ */
+
+/*===========================================================================*/
+/*===============================[ includes ]===============================*/
+/*===========================================================================*/
+#include "TestUtils.h"
+
+/*===========================================================================*/
+/*===============================[ includes ]===============================*/
+/*===========================================================================*/
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <random>
+#include "../src/OrderUtils.h"
+
+std::map<std::string,int> testUtils::getOpenPosition(const OrderBook& orderbook) 
+{
+    std::map<std::string, int> result;
+
+    auto buy_orders = orderutils::getBuyOrders(orderbook);
+    
+    for_each(buy_orders.begin(), buy_orders.end(), [&result](auto& order) {
+            auto res = result.find(order.ticker_);
+            if (res != result.end()) {
+            res->second += order.quantity_;
+            }
+            else {
+            result[order.ticker_] = order.quantity_;
+            }
+            });
+
+    auto sell_orders = orderutils::getSellOrders(orderbook);
+    for_each(sell_orders.begin(), sell_orders.end(), [&result](auto& order) {
+            auto res = result.find(order.ticker_);
+            if (res != result.end()) {
+            res->second -= order.quantity_;
+            }
+            else {
+            result[order.ticker_] = -1*order.quantity_;
+            }
+            });
+
+    return result;
+} 
+
+std::string testUtils::Random::getRandomString(int size)
+{
+    std::uniform_int_distribution<int> dist(0,25);
+
+    auto pos = dist(generator);
+
+    std::string str;
+    while(size-- > 0) {
+        str += std::string(1, 'A' + pos);
+    }
+
+    return str;
+
+} 
+
+int testUtils::Random::getRandomInt(int min, int max)
+{
+    std::uniform_int_distribution<int> dist(min,max);
+
+    return dist(generator);
+} 
+
+std::string testUtils::Random::getRandomTicker() 
+{
+    return getRandomString(1);
+} 
+
+
